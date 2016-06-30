67b805705096f19116b279347daec53652a62639
JB Cayrou <jb.cayrou@gmail.com>
2016-06-30 15:46:18 +0200

    Change argument names + add --account option

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ It is based on miasm and needs EVM architecture implementation https://github.co
 
 Example:
 ```python
-python ethre/ethre.py ./example/in.hex ./mygraph.dot -hex
+python ethre/ethre.py -x./example/in.hex -o ./mygraph.dot
 xdot ./mygraph.dot
+
+python ethre/ethre.py --account 0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae -o ./mygraph.dot
 ```
 
 
@@ -15,12 +17,12 @@ Features
 ---------
 * Disassemble EVM bytecode from binary or from hexadecimal representation
 * Resolve simple JUMP/JUMPI (search PUSH xxxxx JUMP pattern)
+* Get bytecode from ethereum blockchain (`--account` argument)
 
 
 TODO
 -----
 * Add support of text asm
-* Get bytecode from ethereum blockchain
 * Add Semantic analysis (need improve the miasm implementation)
 * Detect functions with contract ABI declaration
 * Execution with Miasm emulation
--- ethre/ethre.py
+++ ethre/ethre.py
@@ -6,6 +6,7 @@ import sys
 from miasm2.core import parse_asm, asmbloc
 from miasm2.arch.evm.disasm import dis_evm, cb_evm_funcs
 from miasm2.arch.evm.arch import mn_evm # Need : https://github.com/jbcayrou/miasm/tree/evm
+from miasm2.arch.evm.env import evm_env
 from miasm2.analysis.machine import Machine
 from miasm2.analysis.binary import Container
 
@@ -16,6 +17,14 @@ class ethRE:
     def __init__(self):
         self.machine = Machine("evm")
         self.mn = self.machine.mn
+
+    def get_bytecode(self, account_addr):
+        code = evm_env.code(int(account_addr[2:],16))
+        code = code[2:] # To remove '0x'..
+        if len(code) % 2 == 1:
+            code  = "0"+code
+        code = binascii.unhexlify(code)
+        return code
 
     def from_bytecode(self, bytecode):
 
@@ -29,35 +38,42 @@ class ethRE:
         self.blks = all_bloc
         raise Exception("Not correctly implemented")
 
-    def graph(self, out_file):
+    def graph(self):
         if not self.blks:
             raise Exception("Need to parse bytecode before")
-        open(out_file, "w").write(self.blks.dot())
+        return self.blks.dot()
 
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ethRE an Ethereum EVM Reverse Engineering tool')
-    parser.add_argument("in_file",help="Binary bytecode to analyze")
-    parser.add_argument("-asm", action="store_true", help="If input is test assembly")
-    parser.add_argument("-hex", action="store_true", help="If input is in hexa")
-    parser.add_argument("out_file",help="output file to save the dot graph")
+    parser.add_argument('-i','--file',help="Input binary bytecode file to analyze")
+    parser.add_argument("-x" ,"--file-hex",help="Input binary bytecode file to analyze in ascii hexadecimal format")
+    parser.add_argument("-a" ,"--account",help="Ethereum contract account to get the bytecode")
+    parser.add_argument("-o" ,"--output",help="output file to save the dot graph")
 
     args = parser.parse_args()
-
-    f = open(args.in_file)
-    code = f.read()
-    code = code.strip()
-
-    if(args.hex):
-        code = binascii.unhexlify(code)
-
     re = ethRE()
 
-    if(args.asm):
-        re.from_asm(code)
-    else:
-        re.from_bytecode(code)
+    if args.file:
+        f = open(args.file)
+        code = f.read()
+        code = code.strip()
 
-    re.graph(args.out_file)
+    if args.file_hex:
+        f = open(args.file_hex)
+        code = f.read()
+        code = code.strip()
+        code = binascii.unhexlify(code)
+    elif args.account:
+        code = re.get_bytecode(args.account)
+
+    re.from_bytecode(code)
+
+    g = re.graph()
+
+    if args.output:
+        open(args.output, "w").write(g)
+    else:
+        print g
